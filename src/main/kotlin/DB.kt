37016cf80147c7925a3bf28c9e480ee7f25c464f
myrdstom <nserekopaul@gmail.com>
2020-11-27 20:16:53 +0300

    first commit

---
+++ src/main/kotlin/DB.kt
@@ -0,0 +1,17 @@
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+
+
+object DB{
+    private val host = "localhost"
+    private val port = 5555
+    private val dbName = "cats_db"
+    private val dbUser = "cats_user"
+    private val dbPassword = "catspass123"
+    fun connect() {
+        val db = Database.connect(
+            "jdbc:postgresql://$host:$port/$dbName", driver = "org.postgresql.Driver",
+            user = dbUser, password = dbPassword
+        )
+    }
+}
---
+++ src/main/kotlin/cats/DB.kt
@@ -0,0 +1,9 @@
+package cats
+
+import org.jetbrains.exposed.dao.IntIdTable
+
+object Cats: IntIdTable(){
+    val name = varchar("name", 20).uniqueIndex()
+    val age = integer("age").default(0)
+
+}
